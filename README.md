241db63a2061748ff0726989be530cb09724cb7d
Chenna Kautilya <chenna@outlook.com>
2018-08-17 18:22:47 -0600

    updated gitignore

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # algorithms
 Implementation of Algorithms and Data Structures from The Algorithm Design Manual by Steven S Skiena
+
+
+### Sorting and Searching
+
+- [Sorting](./src/sorting.cpp)
+- [Searching](./src/searching.cpp)
---
+++ src/sorting.cpp
@@ -0,0 +1,12 @@
+/** Implementing sorting algorithms from The Algorithm Design Manual
+ * In this file we will be implementing:
+ *   - Heap Sort
+ *   - Merge Sort
+ *   - Quick Sort
+*/
+#include "sorting.hpp"
+
+
+int main() {
+    
+}
